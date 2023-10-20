ec9ba8ba9c5d7efd980852d84e37174ae1ffffc7
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-10-20 11:31:54 -0300

    Cadastrando usuário

--- barber/src/routes.ts
+++ barber/src/routes.ts
@@ -1,10 +1,17 @@
 import { Router, Request, Response } from 'express';
 
+import { CreateUserController } from './controllers/user/CreateUserController';
+
 const router = Router();
 
-router.get('/teste', (req: Request, res: Response) => {
+// router.get('/teste', (req: Request, res: Response) => {
     
-    return res.send('Hello World! True 2');
-});
+//     return res.send('Hello World! True 2');
+// });
+
+// --- ROTAS DE USUÁRIO ---
+router.post('/users', new CreateUserController().handle);
+
+
 
 export { router };
