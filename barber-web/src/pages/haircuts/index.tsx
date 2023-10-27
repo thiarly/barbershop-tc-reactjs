339e04488d4caf430f0cac6e40ed2cd31d6faa8c
Thiarly Cavalcante <thiarly.cavalcante@live.com>
2023-10-27 15:22:30 -0300

    Listando modelos

--- barber-web/src/pages/haircuts/index.tsx
+++ barber-web/src/pages/haircuts/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import { Sidebar } from '../../components/sidebar'
 import {
@@ -14,13 +15,28 @@ import {
 import Link from 'next/link';
 
 import { IoMdPricetag } from 'react-icons/io'
+import { canSSRAuth } from '@/src/utils/canSSRAuth';
+import { setupAPIClient } from '@/src/services/api';
 
 
+interface HaircutsItem{
+    id: string;
+    name: string;
+    price: number | string;
+    status: boolean;
+    user_id: string;
+}
+
+interface HaircutsProps{
+    haircuts: HaircutsItem[];
+}
 
 
-export default function Haircuts(){
+export default function Haircuts( {haircuts}: HaircutsProps ){
 
   const [isMobile] = useMediaQuery("(max-width: 550px)")
+
+  const [haircutList , setHaircutList] = useState<HaircutsItem[]>(haircuts || [])
 
   return(
     <>
@@ -63,34 +79,37 @@ export default function Haircuts(){
 
          </Flex>
 
-
+          {haircutList.map(haircut => (
+            
          <Box w="100%">
-            <Link href="/haircuts/123">
-                <Flex
-                cursor="pointer"
-                w="100%"
-                p={4}
-                bg="barber.400"
-                direction="row"
-                rounded="4"
-                mb={2}
-                justifyContent="space-between"
-                >
+         <Link key={haircut.id}  href={`/haircuts/${haircut.id}`}>
+             <Flex
+             cursor="pointer"
+             w="100%"
+             p={4}
+             bg="barber.400"
+             direction="row"
+             rounded="4"
+             mb={2}
+             justifyContent="space-between"
+             >
 
-                <Flex direction="row" alignItems="center">
-                    <IoMdPricetag size={28} color="#fba931"/>
-                    <Text fontWeight="bold" ml={4} noOfLines={2} color="white">
-                    Corte completo
-                    </Text>
-                </Flex>
+             <Flex direction="row" alignItems="center">
+                 <IoMdPricetag size={28} color="#fba931"/>
+                 <Text fontWeight="bold" ml={4} noOfLines={2} color="white">
+                    {haircut.name}
+                 </Text>
+             </Flex>
 
-                <Text fontWeight="bold" color="white" alignSelf="center">
-                    Preço: R$ 59.90
-                </Text>
+             <Text fontWeight="bold" color="white" alignSelf="center">
+                Preço: R$ {haircut.price}
+             </Text>
 
-                </Flex>
-            </Link>
-        </Box>          
+             </Flex>
+         </Link>
+        </Box>
+        ))}
+       
 
 
 
@@ -99,3 +118,38 @@ export default function Haircuts(){
     </>
   )
 }
+
+export const getServerSideProps = canSSRAuth(async (ctx) => {
+    try{
+        const api = setupAPIClient(ctx);
+        const response = await api.get('/haircuts',
+        {
+            params: {
+                status: true,
+            }
+        });
+
+        if (response.data === null){
+            return {
+                redirect: {
+                    destination: '/dashboard',
+                    permanent: false
+            }   }
+        }
+
+        return {
+            props: {
+                haircuts: response.data
+            }
+        }
+
+    }catch(err){
+        console.log(err);
+        return {
+            redirect: {
+                destination: '/dashboard',
+                permanent: false
+            }
+        }
+    }
+})
